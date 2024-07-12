167a053296f8847f88dc9988c28bb31c1cc609b6
Hare Krishna <128279576+tushardhab@users.noreply.github.com>
2024-07-12 18:33:15 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -14,7 +14,7 @@ from plugins.users_api import get_user, update_user_info
 from plugins.database import get_file_details
 from pyrogram.errors import ChatAdminRequired, FloodWait
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, CallbackQuery, InputMediaPhoto
-from config import Var, LOG_CHANNEL, PICS, BATCH_FILE_CAPTION, CUSTOM_FILE_CAPTION, AUTO_DELETE_TIME, AUTO_DELETE, ADMINS
+from config import Var, LOG_CHANNEL, PICS, BATCH_FILE_CAPTION, CUSTOM_FILE_CAPTION, AUTO_DELETE_TIME, AUTO_DELETE, ADMINS, VERIFY, VERIFY_TUTORIAL, BOT_USERNAME
 import re
 import json
 import base64
